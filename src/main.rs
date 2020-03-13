3b3a0e8dbc459a44062fbe7bccce33faa3b131fc
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-13 01:00:13 -0400

    Begin using templating

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use maplit::hashmap;
+use rocket::fairing::Fairing;
 use rocket::{get, routes};
 use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let map = hashmap! {
+        "name" => "connor"
+    };
+    Template::render("main", &map)
+}
+
+macro_rules! register_partial {
+    ($handlebars:expr, $name:literal) => {
+        $handlebars
+            .register_partial($name, include_str!(concat!("../templates/", $name, ".hbs")))
+            .expect(concat!("Count not register partial ", $name, ".hbs"))
+    };
+}
+
+fn configure_templates() -> impl Fairing {
+    Template::custom(|engines| {
+        let handlebars = &mut engines.handlebars;
+        register_partial!(handlebars, "page");
+    })
 }
 
 fn main() {
-    rocket::ignite().mount("/static", StaticFiles::from("static")).mount("/", routes![index]).launch();
+    rocket::ignite()
+        .attach(configure_templates())
+        .mount("/static", StaticFiles::from("static"))
+        .mount("/", routes![index])
+        .launch();
 }
