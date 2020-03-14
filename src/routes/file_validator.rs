8941c96711d340ad2b37b8868edadd624fc9ff53
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-13 23:53:29 -0400

    Improve file validator interface

--- src/routes/file_validator.rs
+++ src/routes/file_validator.rs
@@ -32,7 +32,7 @@ pub enum InputKind {
 
 #[derive(Serialize)]
 pub struct OutputResult {
-    error: bool,
+    errors: u64,
     contents: String,
     warnings: String,
     time: f32,
@@ -42,17 +42,26 @@ fn filter_html(input: impl AsRef<str>) -> String {
     input.as_ref().replace(" ", "&nbsp;").replace("\n", "<br>")
 }
 
-fn process_output(result: &(impl Debug + ?Sized), warnings: impl IntoIterator<Item = impl Debug>) -> (String, String) {
-    (
-        filter_html(format!("{:#?}", result)),
-        filter_html(warnings.into_iter().map(|w| format!("{:?}", w)).join("<br>")),
-    )
+fn process_output(
+    result: &(impl Debug + ?Sized),
+    warnings: impl IntoIterator<Item = impl Debug>,
+) -> (String, String, u64) {
+    let result_string = filter_html(format!("{:#?}", result));
+    let mut count = 0_u64;
+    let warning_string = warnings
+        .into_iter()
+        .map(|w| {
+            count += 1;
+            format!("{:?}", w)
+        })
+        .join("<br>");
+    (result_string, warning_string, count)
 }
 
 #[post("/api/validate-file", data = "<input>")]
 pub fn validate_file(input: Form<InputFile>) -> Json<OutputResult> {
     let start = Instant::now();
-    let (result, warnings) = match input.kind {
+    let (result, warnings, count) = match input.kind {
         InputKind::B3D => {
             let parsed = bve::parse::mesh::mesh_from_str(&input.contents, FileType::B3D);
             process_output(&parsed, parsed.errors.clone())
@@ -84,7 +93,7 @@ pub fn validate_file(input: Form<InputFile>) -> Json<OutputResult> {
     };
     let diff = Instant::now() - start;
     Json(OutputResult {
-        error: !warnings.is_empty(),
+        errors: count,
         contents: result,
         warnings,
         time: diff.as_secs_f32(),
