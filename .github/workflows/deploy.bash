763db8495fa6c8c3465d75532007267ba21a4d42
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-15 14:04:04 -0400

    Autoprune old images

--- .github/workflows/deploy.bash
+++ .github/workflows/deploy.bash
@@ -5,4 +5,5 @@ docker pull cwfitzgerald/bve-reborn-site:latest
 docker stop bve-reborn-site
 docker rm bve-reborn-site
 docker run -d --name bve-reborn-site --restart always -p 9004:8000 cwfitzgerald/bve-reborn-site:latest
+docker image prune -f
 ENDFILE
